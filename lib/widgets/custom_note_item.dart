b8dcaef9993f5213352805d49a3c8bcc77255725
YazanMtawej <yznmtwj@gmail.com>
2024-10-02 22:41:06 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_notes_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-     
-      padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16,),
-      decoration: BoxDecoration(
-        color: Colors.orange,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(fontSize: 26, color: Colors.black),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text("Build your career with yazan mtawej",
-                  style: TextStyle(
-                      fontSize: 18, color: Colors.black.withOpacity(.5))),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                  size: 24,
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              "May 21 , 2024",
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 16,
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return const EditNotesView();
+        }));
+      },
+      child: Container(
+       
+        padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16,),
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
+                style: TextStyle(fontSize: 26, color: Colors.black),
               ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16,bottom: 16),
+                child: Text("Build your career with yazan mtawej",
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.black.withOpacity(.5))),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  )),
             ),
-           ),
-       ], 
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(
+                "May 21 , 2024",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.4),
+                  fontSize: 16,
+                ),
+              ),
+             ),
+         ], 
+        ),
       ),
     );
   }
