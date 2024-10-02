c65b8d8e8d694eed5a703d3ed581d213b2380caa
YazanMtawej <yznmtwj@gmail.com>
2024-10-02 15:56:44 +0300

    create custom text field

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/cutom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [
+        SizedBox(height: 32,),
+        CustomTextField(),
+      ],),
+    );
   }
 }
